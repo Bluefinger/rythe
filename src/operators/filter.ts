111e52433c0bdc49cd128973f7fcefe32b0c8c6b
Bluefinger <bluefinger@gmail.com>
2019-05-14 17:48:27 +0200

    More typings for filter

--- src/operators/filter.ts
+++ src/operators/filter.ts
@@ -2,11 +2,16 @@ import { SKIP } from "../signal";
 import { Cell, OperatorFn } from "../types";
 import { map } from "./map";
 
+export function filter<T, U extends T>(
+  predicate: (value: T) => value is U
+): OperatorFn<T, U>;
+export function filter<T>(predicate: (value: T) => boolean): OperatorFn<T, T>;
+
 /**
  * Filters emitted values using a predicate function to determine what is
  * allowed and what is not.
  */
-export function filter<T>(filterFn: (value: T) => boolean): OperatorFn<T, T> {
+export function filter<T>(predicate: (value: T) => boolean): OperatorFn<T, T> {
   return (source: Cell<T>) =>
-    map<T>(value => (filterFn(value) ? value : SKIP))(source);
+    map<T>(value => (predicate(value) ? value : SKIP))(source);
 }

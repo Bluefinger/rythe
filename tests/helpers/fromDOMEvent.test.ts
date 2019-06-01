15f5d8afc7dd5f8d9549c6b3e0a3db337b280255
Bluefinger <bluefinger@gmail.com>
2019-06-01 10:26:15 +0200

    Helper Function 2, fromDOMEvent

---
+++ tests/helpers/fromDOMEvent.test.ts
@@ -0,0 +1,64 @@
+/**
+ * @jest-environment jsdom
+ */
+import { fromDOMEvent } from "rythe/helpers";
+import { isStream } from "rythe/stream";
+import { StreamState } from "rythe/constants";
+
+describe("fromDOMEvent", () => {
+  it("should return a stream", () => {
+    const element = document.createElement("div");
+    const s = fromDOMEvent(element, "click");
+    expect(isStream(s)).toBe(true);
+    expect(s.state).toBe(StreamState.PENDING);
+  });
+  it("should resolve events from a single DOM element", () => {
+    const element = document.createElement("div");
+    const s = fromDOMEvent(element, "click");
+    const clickEvent = new Event("click");
+    element.dispatchEvent(clickEvent);
+
+    expect(s()).toBe(clickEvent);
+    expect(s().type).toBe("click");
+    expect(s().target).toBe(element);
+    expect(s.state).toBe(StreamState.ACTIVE);
+  });
+  it("should resolve events from many DOM elements", () => {
+    const element = document.createElement("div");
+    element.innerHTML = "<div></div><div></div>";
+    const elements = element.children;
+    const s = fromDOMEvent(elements, "click");
+    const clickEvent = new Event("click");
+    element.children[1].dispatchEvent(clickEvent);
+
+    expect(s()).toBe(clickEvent);
+    expect(s().target).toBe(element.children[1]);
+    expect(s.state).toBe(StreamState.ACTIVE);
+  });
+  it("should remove its event listener from a single DOM element on END", () => {
+    const element = document.createElement("div");
+    const s = fromDOMEvent(element, "click");
+
+    s.end(true);
+
+    const clickEvent = new Event("click");
+    element.dispatchEvent(clickEvent);
+
+    expect(s()).toBeUndefined();
+    expect(s.state).toBe(StreamState.CLOSED);
+  });
+  it("should remove its event listener from many DOM elements on END", () => {
+    const element = document.createElement("div");
+    element.innerHTML = "<div></div><div></div>";
+    const elements = element.children;
+    const s = fromDOMEvent(elements, "click");
+
+    s.end(true);
+
+    const clickEvent = new Event("click");
+    element.children[1].dispatchEvent(clickEvent);
+
+    expect(s()).toBeUndefined();
+    expect(s.state).toBe(StreamState.CLOSED);
+  });
+});

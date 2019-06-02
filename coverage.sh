23c0469b3949748e261b6507e8fe409dd90750d2
Bluefinger <bluefinger@gmail.com>
2019-06-02 11:31:01 +0200

    Further travis matrix fixes, no build on install

--- coverage.sh
+++ coverage.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 coverageCmd() {
   curl -s https://codecov.io/bash -o ./codecov.sh
 }
-if $COVERAGE; then
+
+if [ $COVERAGE ] ; then
   echo "Preparing coverage upload script..."
   coverageCmd
   /bin/bash ./codecov.sh

00a26b86e935d74a48242dac82d5e8dc4183066e
Bluefinger <bluefinger@gmail.com>
2020-06-08 20:10:44 +0200

    remove redundant typing

--- src/signal.ts
+++ src/signal.ts
@@ -10,7 +10,7 @@ export const SKIP = freeze(create(null)) as SKIP;
 /**
  * END. Used for signalling a Stream to close itself when passed as a parameter.
  */
-export const END: END = freeze(create(null)) as END;
+export const END = freeze(create(null)) as END;
 
 export const emitSKIP = <T>(): T => SKIP as T;
 export const emitEND = <T>(): T => END as T;
